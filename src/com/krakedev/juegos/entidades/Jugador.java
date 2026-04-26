d287a78bb15ccec8454ca947794e25ddba47a654
angel-hub31 <moralesmales1@hotmail.com>
2026-04-26 15:20:55 -0500

    se modifica y se crea el metodo imprimir en la clase Jugador

--- src/com/krakedev/juegos/entidades/Jugador.java
+++ src/com/krakedev/juegos/entidades/Jugador.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 
 public class Jugador {
 	private String nicname;
-	private ArrayList<Carta> cartas=new ArrayList<>();
-	
+	private ArrayList<Carta> cartas = new ArrayList<>();
 
 	public void recibirCarta(Carta carta) {
 		cartas.add(carta);
-		
+
+	}
+
+	public void imprimir() {
+		System.out.println("Jugador: " + nicname);
+		for (Carta carta : cartas) {
+			carta.imprimir();
+		}
+
 	}
 }
