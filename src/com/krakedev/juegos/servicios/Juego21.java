473729b12770e39c1e48137e704ce552b55c6dd9
angel-hub31 <moralesmales1@hotmail.com>
2026-04-26 16:00:02 -0500

    Se crea el metodo validarGanador

--- src/com/krakedev/juegos/servicios/Juego21.java
+++ src/com/krakedev/juegos/servicios/Juego21.java
@@ -6,62 +6,69 @@ import com.krakedev.juegos.entidades.Carta;
 import com.krakedev.juegos.entidades.Jugador;
 
 public class Juego21 {
-	private ArrayList<Jugador> jugadores=new ArrayList<>();
+	private ArrayList<Jugador> jugadores = new ArrayList<>();
 	private Dealer dealer;
-	
+
 	public Dealer getDealer() {
-	    return dealer;
+		return dealer;
 	}
-	
-	
+
 	public void cargarValores() {
-		
+
 		ArrayList<Carta> naipes = dealer.getNaipe();
-        for (Carta carta : naipes) {
-            String valor = carta.getValor();
-            if (valor.equals("A")) {
-                carta.setValorJuego(11); 
-            } else if (valor.equals("J") || valor.equals("Q") || valor.equals("K")) {
-                carta.setValorJuego(10); 
-            } else {
-                carta.setValorJuego(Integer.parseInt(valor));
-            }
-        }
-    }
+		for (Carta carta : naipes) {
+			String valor = carta.getValor();
+			if (valor.equals("A")) {
+				carta.setValorJuego(11);
+			} else if (valor.equals("J") || valor.equals("Q") || valor.equals("K")) {
+				carta.setValorJuego(10);
+			} else {
+				carta.setValorJuego(Integer.parseInt(valor));
+			}
+		}
+	}
+
 	public void inicializar() {
-        dealer = new Dealer(); 
-        cargarValores();       
-    }
-	
+		dealer = new Dealer();
+		cargarValores();
+	}
+
 	public void agregarJugador(Jugador jugador) {
-        jugadores.add(jugador); 
-    }
-	
+		jugadores.add(jugador);
+	}
+
 	public void repartirCarta(Jugador jugador) {
-        Carta cartaEntregada = dealer.entregarCarta(); 
-        jugador.recibirCarta(cartaEntregada);          
-    }
-	
+		Carta cartaEntregada = dealer.entregarCarta();
+		jugador.recibirCarta(cartaEntregada);
+	}
+
 	public void repartirRonda() {
-        for (Jugador jugador : jugadores) { 
-            repartirCarta(jugador);         
-        }
-        calcularTotal();
+		for (Jugador jugador : jugadores) {
+			repartirCarta(jugador);
+		}
+		calcularTotal();
 	}
-	
+
 	public void calcularTotal() {
-		for (Jugador jugador : jugadores) { 
-	        int totalPorJugador = 0;
-	        
-	        for (Carta carta : jugador.getCartas()) {
-	            totalPorJugador += carta.getValorJuego();
-	        }
-	        
-	        jugador.setPuntajeCartas(totalPorJugador);
-	    }
+		for (Jugador jugador : jugadores) {
+			int totalPorJugador = 0;
+
+			for (Carta carta : jugador.getCartas()) {
+				totalPorJugador += carta.getValorJuego();
+			}
+
+			jugador.setPuntajeCartas(totalPorJugador);
+		}
 	}
-	
-	
+
+	public ArrayList<Jugador> validarGanador() {
+		ArrayList<Jugador> ganadores = new ArrayList<>();
+
+		for (Jugador jugador : jugadores) {
+			if (jugador.getPuntajeCartas() == 21) {
+				ganadores.add(jugador);
+			}
+		}
+		return ganadores;
+	}
 }
-
-
