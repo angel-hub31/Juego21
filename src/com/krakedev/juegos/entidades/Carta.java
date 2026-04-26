5b91a89ae5fea27a3825e4a5591c696a258d4c2f
angel-hub31 <moralesmales1@hotmail.com>
2026-04-26 13:30:44 -0500

    se crea la clase Carta con sus atributos, se crea constructoresvacio y otro con parametro y tambien se crea los setters y getters

---
+++ src/com/krakedev/juegos/entidades/Carta.java
@@ -0,0 +1,43 @@
+package com.krakedev.juegos.entidades;
+
+public class Carta {
+	private String valor;
+	private int valorJuego;
+	private String palo;
+	
+	
+	//constructor vacio
+	public Carta() {
+		
+	}
+	//constructor con parametros
+	public Carta(String valor, String palo) {
+		
+		this.valor = valor;
+		this.palo = palo;
+	}
+	
+	//
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public int getValorJuego() {
+		return valorJuego;
+	}
+	public void setValorJuego(int valorJuego) {
+		this.valorJuego = valorJuego;
+	}
+	public String getPalo() {
+		return palo;
+	}
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+	
+	
+	
+
+}
