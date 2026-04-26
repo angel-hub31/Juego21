8089b9dbe03e69fe106e621fadc581c02f5d6fa8
angel-hub31 <moralesmales1@hotmail.com>
2026-04-26 14:06:46 -0500

    se crea el metodo generarNaipe

--- src/com/krakedev/juegos/servicios/Dealer.java
+++ src/com/krakedev/juegos/servicios/Dealer.java
@@ -7,6 +7,28 @@ import com.krakedev.juegos.entidades.Carta;
 public class Dealer {
 	private ArrayList<Carta>naipe;
 	
+	//metodo generar naipe 
+	public void generarNaipe() {
+		ArrayList<String> palos=new ArrayList<>();
+		palos.add("T");
+		palos.add("CN");
+		palos.add("CR");
+		palos.add("D");
+		
+		String [] valores = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+			for (String palo :palos) {
+				for(String valor : valores) {
+					Carta carta=new Carta();
+					carta.setValor(valor);
+					carta.setPalo(palo);
+					
+					naipe.add(carta);
+				}
+			}
+		}
+		
+	
+	
 	
 //getters y setter
 	public ArrayList<Carta> getNaipe() {
@@ -16,6 +38,8 @@ public class Dealer {
 	public void setNaipe(ArrayList<Carta> naipe) {
 		this.naipe = naipe;
 	}
+	  
+	
 	
 	
 	
