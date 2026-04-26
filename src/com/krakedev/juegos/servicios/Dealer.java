1919388e8e0f5412ff749a6d62a722fcb6593b6c
angel-hub31 <moralesmales1@hotmail.com>
2026-04-26 13:45:38 -0500

    Se crea los getters y setters

--- src/com/krakedev/juegos/servicios/Dealer.java
+++ src/com/krakedev/juegos/servicios/Dealer.java
@@ -1,5 +1,23 @@
 package com.krakedev.juegos.servicios;
 
+import java.util.ArrayList;
+
+import com.krakedev.juegos.entidades.Carta;
+
 public class Dealer {
+	private ArrayList<Carta>naipe;
+	
+	
+//getters y setter
+	public ArrayList<Carta> getNaipe() {
+		return naipe;
+	}
+
+	public void setNaipe(ArrayList<Carta> naipe) {
+		this.naipe = naipe;
+	}
+	
+	
+	
 
 }
